c87fce20a24da673425ffab062aa7e3c1f4c6e22
dustns09060-eng <dustns09060@gmail.com>
2026-09-08 12:54:58 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ let memberAuthGenerationV133 = 0; // V133: 오래된 세션 검증 요청이 새
 const MEMBER_SESSION_KEY = "yeowoobang:memberSession:v1";
 let securityVersion = "";
 let noticeSignature = "";
-const APP_VERSION = "V226";
-window.YEOWOOBANG_BUILD = "V226";
+const APP_VERSION = "V227";
+window.YEOWOOBANG_BUILD = "V227";
 
 let config = {
   version: "V102",
